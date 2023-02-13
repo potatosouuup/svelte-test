e4ede1436e237c64be0d4bb5ac1e90b6cb884bc3
potatosouuup <85465265+potatosouuup@users.noreply.github.com>
2023-02-13 13:49:40 +0900

    feat: Create svelte test

---
+++ storybook-main/src/pages/index.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let count = 0;
+
+	const a = 0;
+
+	const clickHandler = () => {
+		count += 1;
+	};
+</script>
+
+<p>{count}</p>
+<div>
+	<button on:click="{clickHandler}" class="">button</button>
+</div>
